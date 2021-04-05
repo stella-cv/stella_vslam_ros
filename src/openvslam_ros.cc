756e451a0df673ae4b7ba98cd006ecdad5621497
mirellameelo <miremelo@gmail.com>
2021-04-05 10:44:50 -0700

    updating files to fix frame transfomation

--- src/openvslam_ros.cc
+++ src/openvslam_ros.cc
@@ -12,7 +12,9 @@ system::system(const std::shared_ptr<openvslam::config>& cfg, const std::string&
     : SLAM_(cfg, vocab_file_path), cfg_(cfg), node_(std::make_shared<rclcpp::Node>("run_slam")), custom_qos_(rmw_qos_profile_default),
       mask_(mask_img_path.empty() ? cv::Mat{} : cv::imread(mask_img_path, cv::IMREAD_GRAYSCALE)),
       pose_pub_(node_->create_publisher<nav_msgs::msg::Odometry>("~/camera_pose", 1)),
-      map_broadcaster_(std::make_shared<tf2_ros::TransformBroadcaster>(node_)) {
+      map_to_odom_broadcaster_(std::make_shared<tf2_ros::TransformBroadcaster>(node_)),
+      tf_(std::make_unique<tf2_ros::Buffer>(node_->get_clock())),
+      transform_listener_(std::make_shared<tf2_ros::TransformListener>(*tf_)){
     custom_qos_.depth = 1;
     exec_.add_node(node_);
 }
@@ -36,8 +38,8 @@ void system::publish_pose() {
     // Create odometry message and update it with current camera pose
     nav_msgs::msg::Odometry pose_msg;
     pose_msg.header.stamp = node_->now();
-    pose_msg.header.frame_id = "map";
-    pose_msg.child_frame_id = "camera_link";
+    pose_msg.header.frame_id = map_frame_;
+    pose_msg.child_frame_id = camera_link_;
     pose_msg.pose.pose.orientation.x = quat.x();
     pose_msg.pose.pose.orientation.y = quat.y();
     pose_msg.pose.pose.orientation.z = quat.z();
@@ -47,20 +49,50 @@ void system::publish_pose() {
     pose_msg.pose.pose.position.z = trans(2);
     pose_pub_->publish(pose_msg);
 
-    // Create transform message between map->camera_link
-    geometry_msgs::msg::TransformStamped map_trans;
-    map_trans.header.stamp =  node_->now();
-    map_trans.header.frame_id = "map";
-    map_trans.child_frame_id = "camera_link";
-    map_trans.transform.rotation.x = quat.x();
-    map_trans.transform.rotation.y = quat.y();
-    map_trans.transform.rotation.z = quat.z();
-    map_trans.transform.rotation.w = quat.w();
-    map_trans.transform.translation.x = trans(0);
-    map_trans.transform.translation.y = trans(1);
-    map_trans.transform.translation.z = trans(2);
-    map_broadcaster_->sendTransform(map_trans);
+    // Send map->odom transform. Set publish_tf_ to false with not using TF
+    if(publish_tf_){
 
+        tf2::Stamped<tf2::Transform> camera_to_map(tf2::Transform(tf2::Quaternion(quat.x(), quat.y(), quat.z(), quat.w()), 
+                                                    tf2::Vector3(trans(0), trans(1), trans(2))).inverse(),
+                                                    tf2_ros::fromMsg(node_->now()), camera_link_);
+
+        geometry_msgs::msg::TransformStamped camera_to_map_msg, odom_to_map_msg, map_to_odom_msg;
+        tf2::Stamped<tf2::Transform> odom_to_map_stamped, map_to_odom_stamped;
+
+        // camera_to_map_msg = tf2::toMsg(camera_to_map); - it breaks the execution
+        camera_to_map_msg.header.stamp = tf2_ros::toMsg(camera_to_map.stamp_);
+        camera_to_map_msg.header.frame_id = camera_to_map.frame_id_;
+        camera_to_map_msg.transform.translation.x = camera_to_map.getOrigin().getX();
+        camera_to_map_msg.transform.translation.y = camera_to_map.getOrigin().getY();
+        camera_to_map_msg.transform.translation.z = camera_to_map.getOrigin().getZ();
+        camera_to_map_msg.transform.rotation = tf2::toMsg(camera_to_map.getRotation());
+        
+        try {
+            odom_to_map_msg = tf_->transform(camera_to_map_msg, odom_frame_);
+            tf2::fromMsg(odom_to_map_msg, odom_to_map_stamped);
+            map_to_odom_ = tf2::Transform(tf2::Quaternion(odom_to_map_stamped.getRotation()), tf2::Vector3(odom_to_map_stamped.getOrigin())).inverse();
+
+            tf2::Stamped<tf2::Transform> map_to_odom_stamped(map_to_odom_, tf2_ros::fromMsg(node_->now()), map_frame_);
+
+            map_to_odom_broadcaster_->sendTransform(tf2::toMsg(map_to_odom_stamped));
+
+        }
+        catch (tf2::TransformException & ex) {
+            RCLCPP_ERROR(node_->get_logger(), "Transform failed: %s", ex.what());
+        }
+    }
+}
+
+void system::setParams(){
+    map_to_odom_.setIdentity();
+    odom_frame_ = std::string("odom");
+    odom_frame_ = node_->declare_parameter("odom_frame", odom_frame_);
+
+    map_frame_ = std::string("map");
+    map_frame_ = node_->declare_parameter("map_frame", map_frame_);
+
+    publish_tf_ = true;
+    publish_tf_ = node_->declare_parameter("publish_tf_bool", publish_tf_);
 }
 
 mono::mono(const std::shared_ptr<openvslam::config>& cfg, const std::string& vocab_file_path, const std::string& mask_img_path)
@@ -69,6 +101,7 @@ mono::mono(const std::shared_ptr<openvslam::config>& cfg, const std::string& voc
         node_.get(), "camera/image_raw", [this](const sensor_msgs::msg::Image::ConstSharedPtr& msg) { callback(msg); }, "raw", custom_qos_);
 }
 void mono::callback(const sensor_msgs::msg::Image::ConstSharedPtr& msg) {
+    camera_link_ = msg->header.frame_id;
     const rclcpp::Time tp_1 = node_->now();
     const double timestamp = tp_1.seconds();
 
@@ -93,6 +126,7 @@ stereo::stereo(const std::shared_ptr<openvslam::config>& cfg, const std::string&
 }
 
 void stereo::callback(const sensor_msgs::msg::Image::ConstSharedPtr& left, const sensor_msgs::msg::Image::ConstSharedPtr& right) {
+    camera_link_ = left->header.frame_id;
     auto leftcv = cv_bridge::toCvShare(left)->image;
     auto rightcv = cv_bridge::toCvShare(right)->image;
     if (leftcv.empty() || rightcv.empty()) {
@@ -125,6 +159,7 @@ rgbd::rgbd(const std::shared_ptr<openvslam::config>& cfg, const std::string& voc
 }
 
 void rgbd::callback(const sensor_msgs::msg::Image::ConstSharedPtr& color, const sensor_msgs::msg::Image::ConstSharedPtr& depth) {
+    camera_link_ = color->header.frame_id;
     auto colorcv = cv_bridge::toCvShare(color)->image;
     auto depthcv = cv_bridge::toCvShare(depth)->image;
     if (colorcv.empty() || depthcv.empty()) {
