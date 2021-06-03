8ed25286d02ab81afb97a34acf2a9dee8f675817
Alexey Merzlyakov <60094858+AlexeyMerzlyakov@users.noreply.github.com>
2021-06-03 15:35:38 +0300

    Fix headers for Galactic (#24)

--- src/openvslam_ros.h
+++ src/openvslam_ros.h
@@ -16,6 +16,7 @@
 
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/buffer.h>
 
 #include <opencv2/core/core.hpp>
 #include <sensor_msgs/msg/image.hpp>
